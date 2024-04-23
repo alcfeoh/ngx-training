da69b4cc5f9f8b21eb6d1c826e99102098e13710
alcfeoh <alain@interstate21.com>
2024-04-23 13:12:55 -0700

    LicensePlate service using signals

--- src/app/license-plate.service.ts
+++ src/app/license-plate.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable, Signal} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LicensePlate} from './license-plate';
+import {Observable} from 'rxjs';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LicensePlateService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+  private licensePlates$ = this.http.get<LicensePlate[]>('http://localhost:8000/data');
+  readonly licensePlates = toSignal(this.licensePlates$, {initialValue: []});
+
+  getPlates(): Signal<LicensePlate[]> {
+    return this.licensePlates;
+  }
+
 }
