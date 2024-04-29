a52e986f3dc8e475eab099b40b1db35fdca38334
alcfeoh <alain@interstate21.com>
2024-04-29 11:23:05 -0700

    Added CartService support as a GraphQL based option

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,25 +1,56 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {inject, Injectable} from '@angular/core';
+import {BehaviorSubject, Observable, of, switchMap} from 'rxjs';
 import {LicensePlate} from './license-plate';
-import {HttpClient} from '@angular/common/http';
+import {Apollo, gql} from 'apollo-angular';
+import {map, tap} from 'rxjs/operators';
+
+const GET_CART_CONTENTS = gql`
+  {
+   cart {
+      _id
+      description
+      picture
+      price
+      title
+    }
+  }
+`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
-  constructor(private http: HttpClient) { }
+  private apollo = inject(Apollo);
+  readonly cartContents$ = new BehaviorSubject<LicensePlate[]>([]);
 
   getCartContents(): Observable<LicensePlate[]> {
-    return this.http.get<LicensePlate[]>('http://localhost:8000/cart');
+    return this.apollo.subscribe<{cart : LicensePlate[]}>({query: GET_CART_CONTENTS})
+    .pipe(
+      map(result => result.data?.cart ?? []),
+      tap(res => this.cartContents$.next(res))
+    );
   }
 
   addToCart(plate: LicensePlate): Observable<unknown> {
-    return this.http.put('http://localhost:8000/cart/' + plate._id, null);
+   const mutation =  gql`
+       mutation AddToCart {
+        addToCart(plateId: "${plate._id}") {
+          _id
+        }
+      }
+  `;
+    return this.apollo.mutate({mutation}).pipe(switchMap(() => this.getCartContents()));
   }
 
   removeFromCart(plate: LicensePlate): Observable<unknown> {
-    return this.http.delete('http://localhost:8000/cart/' + plate._id);
+    const mutation =  gql`
+       mutation removeFromCart {
+        removeFromCart(plateId: "${plate._id}") {
+          _id
+        }
+      }
+  `;
+    return this.apollo.mutate({mutation}).pipe(switchMap(() => this.getCartContents()));
   }
 
 }
