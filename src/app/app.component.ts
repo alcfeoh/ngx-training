2f95540c4465e9bcb0e059b1924ad7850c1ad166
alcfeoh <alain@interstate21.com>
2025-01-22 14:18:29 -0800

    Upgrade to ng19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import {NavigationComponent} from './navigation/navigation.component';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    NavigationComponent,
-    RouterOutlet
-  ],
-  templateUrl: "app.component.html"
+    selector: 'app-root',
+    imports: [
+        NavigationComponent,
+        RouterOutlet
+    ],
+    templateUrl: "app.component.html"
 })
 export class AppComponent {
 }
